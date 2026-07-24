0b283c4f2ec9c65c62443f12f7ef348b36f9c57d
vinodkumar <vinodrao835@gmail.com>
2026-07-24 16:58:27 +0000

    fix(ui): fix filtering search input

--- Frontend/src/components/Layout/AppLayout.jsx
+++ Frontend/src/components/Layout/AppLayout.jsx
@@ -1,21 +1,12 @@
-import TextInput from '../Form/TextInput'
 import Profile from '../../assets/profile.png'
-import { LogOut, Search, LayoutDashboard, Folder, ListChecks, User } from 'lucide-react'
+import { LogOut, LayoutDashboard, Folder, ListChecks, User } from 'lucide-react'
 import SidebarItem from './SidebarItem'
-import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
 
 const AppLayout = () => {
-  const location = useLocation()
   const navigate = useNavigate()
   const { user, logout, loading } = useAuth()
-  const searchConfig = {
-    '/dashboard': 'Search projects...',
-    '/projects': 'Search projects...',
-  }
-
-  const showSearch = location.pathname !== '/profile' && location.pathname !== '/tasks'
-  const searchPlaceholder = searchConfig[location.pathname] ?? 'Search...'
   const profileImage = user?.profilePic || Profile
 
   const handleLogout = async () => {
@@ -31,29 +22,13 @@ const AppLayout = () => {
     <div className="relative min-h-screen bg-[var(--color-background)] text-[var(--color-text)]">
       {/* Header */}
       <header className="fixed left-0 right-0 top-0 z-30 flex justify-between border border-2 border-b-[var(--color-border)] bg-[var(--color-background)] px-8 py-3">
-        <div className="flex items-center gap-20">
+        <div className="flex items-center">
           <Link to="/dashboard" className="flex items-center gap-2">
             <LayoutDashboard className="h-8 w-8 rounded-[var(--radius-xs)] bg-[var(--color-primary)] p-1.5 text-white" />
             <div className="leading-tight">
               <p className="text-[16px] font-bold text-[var(--color-text)]">TeamBoard</p>
             </div>
           </Link>
-
-          {showSearch ? (
-            <div className="relative w-[256px]">
-              <Search className="absolute left-4 top-2 h-4 w-4 text-[var(--color-text-muted)]" />
-              <TextInput
-                id="search"
-                name="search"
-                type="text"
-                value={''}
-                onChange={''}
-                placeholder={searchPlaceholder}
-                required
-                className="pl-[38px]"
-              />
-            </div>
-          ) : null}
         </div>
 
         <Link
